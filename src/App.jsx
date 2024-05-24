1fa11be529fc0f1976f0133916b7736727df79b5
Prem <premkhodke.99@gmail.com>
2024-05-25 00:48:01 +0530

    Digital clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import PaginationTest from './components/Pagination/test'
+import DigitalClock from './components/DigitalClock/DigitalClock'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <div>
         <PaginationTest />
+        <DigitalClock />
       </div>
     </>
   )
